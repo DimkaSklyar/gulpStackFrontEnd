dc2ef55e619864bbbe46dd87fb7b1305f0bb010e
Dmitry <30568388+DimkaSklyar@users.noreply.github.com>
2017-10-09 20:17:26 +0300

    Expanded CSS and logError
    add Expanded CSS and logError

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ var gulp 					= require('gulp'),
 
 gulp.task('sass', function() {
 	return gulp.src('src/sass/*.sass')
-	.pipe(sass())
+	.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
 	.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
 	.pipe(gulp.dest('src/css'))
 	.pipe(browserSync.reload({stream: true}))
